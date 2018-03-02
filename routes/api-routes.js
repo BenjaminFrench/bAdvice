9470c84acda389355a3a4cfe15bf34fc6f1183c6
Ben French <ben.jminw@gmail.com>
2018-03-02 01:54:19 -0700

    Making barebones views. Adding ability to post questions for signed in users.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,14 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.post("/api/questions", function (req, res) {
+    app.post("/api/questions", isLoggedIn, function (req, res) {
         db.Question.create({
             title: req.body.title,
             text: req.body.text,
-            UserId: req.body.UserId
+            UserId: req.user.id
         }).then(dbQuestion => {
-            res.json(dbQuestion);
+            res.redirect('/questions/'+dbQuestion.id);
+            // res.json(dbQuestion);
         });
     });
 
@@ -28,4 +29,16 @@ module.exports = function (app) {
             res.json(dbQuestion);
         });
     });
+
+    app.get("/api/user", function (req, res) {
+        console.log(req.user);
+        res.json(req.user);
+    });
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/signin');
+    }
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,26 @@ var db = require("../models");
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        db.Burger.findAll({}).then(result => {
+        db.Question.findAll({}).then(result => {
             var hbsObject = {
-                burgers: result
+                questions: result
             };
             res.render("index", hbsObject);
+        });
+    });
+
+    app.get("/questions/:id", function (req, res) {
+        db.Question.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(result => {
+            var hbsObject = {
+                title: result.title,
+                text: result.text,
+                id: result.id
+            };
+            res.render("singleQuestion", hbsObject);
         });
     });
 }
