2717cebf95d4525b1269364d0d75a588140b961b
Ben French <ben.jminw@gmail.com>
2018-03-01 18:29:52 -0700

    Initial files

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS badvice_db;
+CREATE database badvice_db;
+USE badvice_db;
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var passport   = require('passport');
+var session    = require('express-session');
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/auth-routes.js")(app, passport);
+
+require('./config/passport/passport.js')(passport, db.user);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
+});
